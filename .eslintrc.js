979e56c1bad9e86da5506059860060cac7736b65
qamarabbas024 <qamarabbas33333333@gmail.com>
2026-08-23 03:20:40 +0500

    chore(eslint): configure monorepo ESLint configurations for clean lint verification

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+  ],
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js', 'dist', '.next', 'node_modules', 'out'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+    'no-useless-escape': 'warn',
+  },
+};
---
+++ frontend/.eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ['../.eslintrc.js'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'no-useless-escape': 'warn',
+  },
+};
